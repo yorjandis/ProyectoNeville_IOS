ae86be48bcef939b0d21dfab5a480071c65f90d4
Yorjandis <yperezg811013@gmail.com>
2023-11-12 04:05:55 +0100

    game

--- Neville_iOS/Views/PLayGame/GamePLay.swift
+++ Neville_iOS/Views/PLayGame/GamePLay.swift
@@ -8,63 +8,151 @@
 import SwiftUI
 
 struct GamePLay: View {
+    @Environment(\.dismiss) var dimiss
+
+
+    @State private var Fallos = 0
+    @State private var Aciertos = 0
     
-    @State private var show = false
-    @State private var trim : Double = 0
-    @State private var percentage = 0
+    @State private var pregunta = lista().listado.randomElement()?.pregunta
     
-    @State private var opacidad : Double  = 1
-    
-    @State var tope = 50
-    
-    let tt = [("Yorjandis",true),("Carlito",false),("Juan", true),("Perez", false)]
     
     var body: some View {
-        ZStack(alignment: .bottom){
-            
-            LinearGradient(colors: [.brown, .orange], startPoint: .bottom, endPoint: .top)
-                .ignoresSafeArea()
-            Text("Yorjandis")
-                .offset(x: Double.random(in: 0...50),y: -CGFloat(percentage * 5) )
-                .opacity(opacidad)
-                .onTapGesture {
-                    withAnimation {
-                        opacidad = 0
-                    }
+        NavigationStack {
+            ZStack(alignment: .bottom){
+                
+                LinearGradient(colors: [.brown, .orange], startPoint: .bottom, endPoint: .top)
+                    .ignoresSafeArea()
+                
+                VStack{
+                    Spacer()
                     
-                }
-     
-                Circle()
-                .trim(from: 0,to: trim)
-                .stroke(Color.black.opacity(0.7), lineWidth: 40)
-                .rotationEffect(.degrees(180))
-                .padding(.bottom, -228)
-                //.opacity(percentage > 100 + 2 ? 0 : 1)
-            
-            Text(percentage > tope ? "Completado!" : "\(percentage) %")
-                .contentTransition(.numericText(value: Double(percentage)))
-                .font(.system(size: percentage > 20 ? 50 : 40))
-                .foregroundStyle(.black)
-                .bold()
-                //.opacity(percentage > 100 + 2 ? 0 : 1 )
-                .onTapGesture {
-                    Timer.scheduledTimer(withTimeInterval: 0.50, repeats: true) { timer in
-                        withAnimation {  
-                            if percentage <= tope {
-                                percentage += 1
-                                trim += (1/Double(tope)) / 2
+                    Text(pregunta ?? "")
+                        .font(.system(size: 40)).bold()
+                        .fontDesign(.serif)
+                    
+                    Spacer()
+                    
+                    
+                    VStack{
+                        
+                        HStack{
+                            Button{
+                                withAnimation {
+                                    pregunta = lista().listado.randomElement()?.pregunta
+                                }
+                            }label: {
+                                Text("Verdadero")
+                                    .foregroundStyle(.white)
+                                    
                             }
-                            
+                            .frame(width: 100)
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 20, height: 10)))
+                            Button{
+                                withAnimation {
+                                    pregunta = lista().listado.randomElement()?.pregunta
+                                }
+                            }label: {
+                                Text("Falso")
+                                    .foregroundStyle(.white)
+                                    
+                            }
+                            .frame(width: 100)
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 20, height: 10)))
                             
                         }
                         
+                        Button{
+                            withAnimation {
+                                pregunta = lista().listado.randomElement()?.pregunta
+                            }
+                        }label: {
+                            Text("Siguiente")
+                                .foregroundStyle(.white)
+                                
+                        }
+                        .padding()
+                        .background(.black.opacity(0.7))
+                        .clipShape(RoundedRectangle(cornerSize: CGSize(width: 20, height: 10)))
+                    }
+                    
+                    
+                    
+                    
+                    
+                    HStack(spacing: 20){
+                        Text("Fallos:\(Fallos)")
+                            
+                        Text("Aciertos:\(Aciertos)")
+                            
+                    }
+                    .font(.system(size: 30))
+                    .bold()
+                }
+   
                     }
                 }
-
+            }
+    
+    
+    func checkValue(pregunta : Pregunta, buttonSi : Bool ){
+        
+        if buttonSi {
+            if pregunta.isCorrect{
+                
+                Aciertos += 1
+            }else{
+                Fallos += 1
+            }
+        }else{
+            
+            if !pregunta.isCorrect{
+                Aciertos += 1
+            }else{
+                Fallos += 1
+            }
+            
         }
 
     }
+    
+    
+        }
+ 
+    
+
+
+class Pregunta : ObservableObject, Identifiable{
+    var id: UUID = UUID()
+    let pregunta : String
+    let isCorrect : Bool
+    var isVisible : Bool = true
+    
+    init(pregunta: String, isCorrect: Bool) {
+        self.pregunta = pregunta
+        self.isCorrect = isCorrect
+    }
+    
+    
 }
+
+struct lista{
+    
+
+     let listado = [         Pregunta(pregunta: "Imaginar Crea la realidad", isCorrect: true),
+                                   Pregunta(pregunta: "La realidad esta sujeta a nuestros sentidos físicos", isCorrect: false),
+                                   Pregunta(pregunta: "Podemos cambiar el pasado", isCorrect: true),
+                                   Pregunta(pregunta: "La Biblia es un libro de historia antigua", isCorrect: false),
+                                   Pregunta(pregunta: "Jesús Cristo vivió hace 2000 años", isCorrect: false)
+         ]
+    
+}
+
+
 
 #Preview {
     GamePLay()
