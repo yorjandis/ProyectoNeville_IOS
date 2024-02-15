ec2677bd5b8dff9cf1ad096496fdc0e80f75d875
Yorjandis <yperezg811013@gmail.com>
2024-02-15 09:47:33 +0100

    okokook. Ultima actualización de la versión 1.2.0 antes de publicar a app store

---
+++ WatchComplicationLaLey/WatchComplicationLaLey.swift
@@ -0,0 +1,83 @@
+//
+//  WatchComplicationLaLey.swift
+//  WatchComplicationLaLey
+//
+//  Created by Yorjandis Garcia on 15/2/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), emoji: "😀")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+}
+
+struct WatchComplicationLaLeyEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Time:")
+                Text(entry.date, style: .time)
+            }
+
+            Text("Emoji:")
+            Text(entry.emoji)
+        }
+    }
+}
+
+@main
+struct WatchComplicationLaLey: Widget {
+    let kind: String = "WatchComplicationLaLey"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(watchOS 10.0, *) {
+                WatchComplicationLaLeyEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                WatchComplicationLaLeyEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .accessoryRectangular) {
+    WatchComplicationLaLey()
+} timeline: {
+    SimpleEntry(date: .now, emoji: "😀")
+    SimpleEntry(date: .now, emoji: "🤩")
+}
